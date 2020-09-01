a78d92f026111d1e7e3fafa76a9b124058dda46a
perosa100 <patrickperosapp@gmail.com>
2020-09-01 02:17:36 -0400

    feat: should return 422 when there is a validation error

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,9 @@
+import { Beach } from '@src/models/beach'
 describe('Beaches functional tests', () => {
+  beforeAll(async () => {
+    await Beach.deleteMany({})
+  })
+
   describe('When creating a beach', () => {
     it('should create a beach with success', async () => {
       const NewBeach = {
@@ -11,6 +16,23 @@ describe('Beaches functional tests', () => {
       const response = await global.testRequest.post('/beaches').send(NewBeach)
       expect(response.status).toBe(201)
       expect(response.body).toEqual(expect.objectContaining(NewBeach))
+    })
+
+    it('should return 422 when there is a validation error', async () => {
+      const NewBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E'
+      }
+
+      const response = await global.testRequest.post('/beaches').send(NewBeach)
+
+      expect(response.status).toBe(422)
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"'
+      })
     })
   })
 })
