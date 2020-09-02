52e5aff43b439635d9a183235899a152e5dc2b9a
perosa100 <patrickperosapp@gmail.com>
2020-09-02 17:22:22 -0400

    fix: users

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,8 +1,8 @@
-import AuthService from '@src/services/auth'
 import { Controller, Post } from '@overnightjs/core'
-import { Request, Response } from 'express'
+import { Response, Request } from 'express'
 import { User } from '@src/models/user'
-import { BaseController } from '.'
+import AuthService from '@src/services/auth'
+import { BaseController } from './index'
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -10,9 +10,7 @@ export class UsersController extends BaseController {
   public async create(req: Request, res: Response): Promise<void> {
     try {
       const user = new User(req.body)
-
       const newUser = await user.save()
-
       res.status(201).send(newUser)
     } catch (error) {
       this.sendCreateUpdateErrorResponse(res, error)
@@ -21,25 +19,22 @@ export class UsersController extends BaseController {
 
   @Post('authenticate')
   public async authenticate(req: Request, res: Response): Promise<Response> {
-    const { email, password } = req.body
-
-    const user = await User.findOne({ email })
-
+    const user = await User.findOne({ email: req.body.email })
     if (!user) {
       return res.status(401).send({
         code: 401,
         error: 'User not found!'
       })
     }
-
-    if (!(await AuthService.comparePasswords(password, user.password))) {
-      return res.status(401).send({
-        code: 401,
-        error: 'Password does not match'
-      })
+    if (
+      !(await AuthService.comparePasswords(req.body.password, user.password))
+    ) {
+      return res
+        .status(401)
+        .send({ code: 401, error: 'Password does not match!' })
     }
     const token = AuthService.generateToken(user.toJSON())
 
-    return res.status(200).send({ token })
+    return res.send({ ...user.toJSON(), ...{ token } })
   }
 }
